543375b1b84028957cb7d5de1372ab21dacef129
luizbottino <luizbottino@outlook.com>
2023-05-30 11:35:37 -0300

    Adição das pastas Entities e Helper

---
+++ Entities/TodoItemsEntities.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ToDoAPI.Entities
+{
+
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string? name { get; set; }
+        public Boolean? isComplete{ get; set; }
+
+    }
+}
